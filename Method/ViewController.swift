662743e80003ad43f10d5e06e9053b7fd2adbe52
Ryuma.h <ryuma810dragon@gmail.com>
2019-06-05 17:46:19 +0900

    new opt4

--- Method/ViewController.swift
+++ Method/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet var result: UILabel!
+    var numberResult: Int = 0
+    var x1: Int!
+    var x2: Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func calculate(sender: UIButton) {
+        if (self.x1 != nil) || (self.x2 != nil) {
+            var x1: Int = Int(textField1.text!)!
+            var x2: Int = Int(textField2.text!)!
+            
+            
+            numberResult = x1 + x2
+            result.text = String(numberResult)
+        }
+    }
+    
 }
 
