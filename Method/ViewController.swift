a7af82e38344893d45477b01a7cf4e7a80907441
Ryuma.h <ryuma810dragon@gmail.com>
2019-06-05 17:06:07 +0900

    Initial Commit

---
+++ Method/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Method
+//
+//  Created by Ryuma Harada on 2019/06/05.
+//  Copyright © 2019 Ryuma Harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
